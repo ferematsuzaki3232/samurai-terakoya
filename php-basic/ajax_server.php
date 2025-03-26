7b5cf1cfab5be16c37b627d8860e840bf9e84137
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-03-26 19:31:07 +0900

    php-basic　更新しました

--- php-basic/ajax_server.php
+++ php-basic/ajax_server.php
@@ -8,28 +8,30 @@
 <body>
     <p>
     <?php
-// Ajaxリクエストを取得
-$ajax_request = file_get_contents('php://input');
+     // Ajaxリクエストを取得
+     $ajax_request = file_get_contents('php://input');
 
-// AjaxリクエストをPHPで扱える連想配列に変換
-$data = json_decode($ajax_request, true);
+     // AjaxリクエストをPHPで扱える連想配列に変換
+     $data = json_decode($ajax_request, true);
 
-// 受け取ったデータに応じて処理を行う
-if ($data['name'] === 'SAMURAI') {
-    $data['name'] = 'TERAKOYA';
-} else {
-    $data['name'] = 'SAMURAI';
-}
+     // 受け取ったデータに応じて処理を行う
+     if ($data['name'] === 'SAMURAI') {
+         $data['name'] = 'TERAKOYA';
+     } else {
+         $data['name'] = 'SAMURAI';
+     }
 
-// Ajaxレスポンスを生成（連想配列としてセット）
-$response = [
+     // Ajaxレスポンスを生成（連想配列としてセット）
+     $response = [
     'message' => $data['name']
-];
+     ];
 
-// JSON形式を指定してブラウザ側へ返信
-header('Content-Type: application/json');
-echo json_encode($response);
-?>
+    // JSON形式を指定してブラウザ側へ返信
+     header('Content-Type: application/json');
+     echo json_encode($response);
+     ?>
     </p>
+    <!-- XAMPP（Windows）：localhost/php-basic/ajax_browser.html -->
+    <!-- MAMP（Mac）：localhost:8888/php-basic/ajax_browser.html -->
 </body>
 </html>
