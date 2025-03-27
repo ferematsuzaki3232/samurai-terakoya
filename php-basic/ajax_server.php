8d06b093458a52d22ed94070b2cf05393d259194
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-03-27 12:13:44 +0900

    php-basic　3.27.1213　更新しました

--- php-basic/ajax_server.php
+++ php-basic/ajax_server.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>
+
     <?php
      // Ajaxリクエストを取得
      $ajax_request = file_get_contents('php://input');
@@ -30,8 +22,5 @@
      header('Content-Type: application/json');
      echo json_encode($response);
      ?>
-    </p>
     <!-- XAMPP（Windows）：localhost/php-basic/ajax_browser.html -->
     <!-- MAMP（Mac）：localhost:8888/php-basic/ajax_browser.html -->
-</body>
-</html>
