b77f337c441955c99050a6ff173e98651c36e6fa
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-04-22 12:45:49 +0900

    php-basic　更新しました

--- php-basic/ajax_server.php
+++ php-basic/ajax_server.php
@@ -1,26 +1,19 @@
+<?php
+// JSON形式を指定してブラウザ側へ返信
+header('Content-Type: application/json');
 
-    <?php
-     // Ajaxリクエストを取得
-     $ajax_request = file_get_contents('php://input');
+// Ajaxリクエストを取得
+$ajax_request = file_get_contents('php://input');
 
-     // AjaxリクエストをPHPで扱える連想配列に変換
-     $data = json_decode($ajax_request, true);
+// JSONをPHPの連想配列に変換
+$data = json_decode($ajax_request, true);
 
-     // 受け取ったデータに応じて処理を行う
-     if ($data['name'] === 'SAMURAI') {
-         $data['name'] = 'TERAKOYA';
-     } else {
-         $data['name'] = 'SAMURAI';
-     }
+// nameの値に応じて切り替え
+if ($data['name'] === 'SAMURAI') {
+    $data['name'] = 'TERAKOYA';
+} else {
+    $data['name'] = 'SAMURAI';
+}
 
-     // Ajaxレスポンスを生成（連想配列としてセット）
-     $response = [
-    'message' => $data['name']
-     ];
-
-    // JSON形式を指定してブラウザ側へ返信
-     header('Content-Type: application/json');
-     echo json_encode($response);
-     ?>
-    <!-- XAMPP（Windows）：localhost/php-basic/ajax_browser.html -->
-    <!-- MAMP（Mac）：localhost:8888/php-basic/ajax_browser.html -->
+// JSONとして出力
+echo json_encode(['message' => $data['name']]);
