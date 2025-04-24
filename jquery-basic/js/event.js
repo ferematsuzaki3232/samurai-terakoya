483a0063afd7a4614ed8d1549dd24e97b6b2049b
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-04-24 20:31:12 +0900

    jquery-basic　更新しました

---
+++ jquery-basic/js/event.js
@@ -0,0 +1,41 @@
+$(function() {
+    // ボタンがクリックされた場合
+    // $('button').on('click', function(){
+    //   console.log('ボタンがクリックされました');
+    // });
+
+
+    // $('div').on({
+    //     'click': () => { /* クリック時の処理 */ },
+    //     'dblclick': () => { /* ダブルクリック時の処理 */ },
+    //     'mouseenter': () => { /* マウスカーソルが入ったときの処理 */ },
+    //     'mouseout': () => { /* マウスカーソルが離れたときの処理 */ },
+    //   });
+    // ↓
+    $('div').on({
+        'click': () => {
+          // 背景を赤 
+          $('div').css('background-color', 'red');
+          // テキストを「click」 
+          $('div').text('click');
+        },
+        'dblclick': () => { 
+          // 背景を緑 
+          $('div').css('background-color', 'green'); 
+          // テキストを「dblclick」 
+          $('div').text('dblclick');
+        },
+        'mouseenter': () => {
+          // 背景を青 
+          $('div').css('background-color', 'blue'); 
+          // テキストを「mouseenter」 
+          $('div').text('mouseenter');
+         },
+         'mouseout': () => {
+          // 背景をグレー 
+          $('div').css('background-color', 'gray'); 
+          // テキストを「mouseout」 
+          $('div').text('mouseout');
+        },
+      });
+   });
